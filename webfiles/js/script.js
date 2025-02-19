083c694ae4b529f4e94e0ebc259ccbbea2b99949
EL KOLLI LYNDA <elkollilynda@gmail.com>
2025-02-19 16:05:02 +0100

    ajout animation section invit

--- webfiles/js/script.js
+++ webfiles/js/script.js
@@ -1,8 +1,20 @@
 const guest = document.querySelector(".guests-container");
 
-
+const invited = document.querySelector(".invit");
 
 
 guest.addEventListener("click", function () {
   guest.classList.toggle("guestOne");
 });
+
+
+
+invited.addEventListener("click", function () {
+    invited.classList.toggle("invitOne");
+  });
+
+  const burger = document.querySelector("#burger-icon");
+  const navMenu = document.querySelector(".mobilenav");
+
+burger.addEventListener("click", function (){
+  navMenu.classList.toggle("open");});

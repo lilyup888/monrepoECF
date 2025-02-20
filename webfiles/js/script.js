234bb7a0eb1746a0c26eb6447b21802bb4cdb4d8
EL KOLLI LYNDA <elkollilynda@gmail.com>
2025-02-20 13:22:59 +0100

    ajout croix

--- webfiles/js/script.js
+++ webfiles/js/script.js
@@ -2,19 +2,78 @@ const guest = document.querySelector(".guests-container");
 
 const invited = document.querySelector(".invit");
 
-
 guest.addEventListener("click", function () {
   guest.classList.toggle("guestOne");
 });
-
-
 
 invited.addEventListener("click", function () {
     invited.classList.toggle("invitOne");
   });
 
-  const burger = document.querySelector("nav button span");
-  const navMenu = document.querySelector(".mobileNav");
 
-burger.addEventListener("click", function (){
-  navMenu.classList.toggle("open");});
+ document.addEventListener("DOMContentLoaded", function() {
+  const menuToggle = document.getElementById("burger-icon");
+
+  menuToggle.addEventListener("click", function() {
+      this.classList.toggle("active"); 
+      
+  });
+});
+const boutton= document.getElementById("burger-icon");
+const hiddenMenu= document.getElementById("#hidden");
+
+
+boutton.addEventListener("click", function() {
+    hidden.classList.toggle("hidden");
+    
+});
+
+hiddenMenu.addEventListener("mouseleave", function () {
+    hiddenMenu.classList.add("hidden");
+});
+
+
+
+
+
+const nameRegex = /^[A-Za-zÀ-ÖØ-öø-ÿ-]+$/;
+
+const nameField = document.getElementById("lastname");
+const fnameField = document.getElementById("firstname");
+const mailField = document.getElementById("mail");
+const objectField = document.getElementById("object");
+const messField = document.getElementById("message");
+
+function validateForm() {
+  const name = nameField.value.trim();
+  const fname = fnameField.value.trim();
+  const email = mailField.value.trim();
+  const object = objectField.value.trim();
+  const message = messField.value.trim();
+  
+ 
+
+  
+  const submitBtn = document.getElementById("submit-btn");
+ 
+
+  if (name !== "" && fname !== "" && object !== "" && message !== "" && agreement.checked && name.length >= 2 && name.length <= 50 && message.length >= 3 && message.length <=250 && nameRegex.test(name) &&
+  nameRegex.test(fname) ) {
+    submitBtn.disabled = false; 
+  } else {
+    submitBtn.disabled = true; 
+  }
+}
+
+
+
+
+
+
+
+nameField.addEventListener("input", validateForm);
+fnameField.addEventListener("input", validateForm);
+mailField.addEventListener("input", validateForm);
+objectField.addEventListener("input", validateForm);
+messField.addEventListener("input", validateForm);
+
